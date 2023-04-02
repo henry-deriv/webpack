e7b8cbefc16e75f2ceba7c8218262877a2bf00da
Henry Hein <henry@regentmarkets.com>
2023-04-02 19:18:45 +0800

    webpack complete config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import generateJoke from "./generateJoke";
 import './style/main.scss';
+import arrow from './assets/ic-arrow-orange-up.svg';
 
-console.log(generateJoke());
+const arrowImg = document.getElementById('arrow');
+arrowImg.src = arrow;
+
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
